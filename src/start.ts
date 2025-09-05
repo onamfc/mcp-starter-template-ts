8815be5942dd80201212c7df11deeebdd7e834ca
Brandon Estrella <onamfc@gmail.com>
2025-09-05 09:24:08 -0700

    run prettier

--- src/start.ts
+++ src/start.ts
@@ -3,7 +3,7 @@ import { main } from './server-core.js';
 /**
  * Run if executed directly (robust ESM check across platforms)
  */
-void main().catch((error) => {
+void main().catch(error => {
   console.error('Unhandled error:', error);
   process.exit(1);
 });
